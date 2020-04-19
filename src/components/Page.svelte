6f06189dd2b6e952f5256b08d3b28450b7201c3b
Gaëtan Rizio <gaetan.rizio+git@pm.me>
2020-04-19 18:26:32 +0200

    Correct page transition

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -1,7 +1,22 @@
 <script>
-  import { fade } from 'svelte/transition'
+  function pageFade(node, {delay = 0, duration = 400, easing}) {
+    const o = +getComputedStyle(node).opacity;
+    return {
+      delay,
+      duration,
+      easing,
+      css: t => `opacity: ${t * o}`,
+      tick: t => {
+        if (t === 0) {
+          node.style.position = "fixed"
+        } else {
+          node.style.position = "static"
+        }
+      }
+    }
+  }
 </script>
 
-<div in:fade="{{ delay: 150, duration: 100 }}" out:fade="{{ duration: 100 }}">
-  <slot />
+<div in:pageFade="{{ delay: 150, duration: 100 }}" out:pageFade="{{ duration: 100 }}">
+  <slot/>
 </div>

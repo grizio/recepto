70ee63546d2f6d2ce6d9941cc8af7f40d18b1379
Gaëtan Rizio <gaetan.rizio+git@pm.me>
2020-05-11 18:09:37 +0200

    Move the external search
    Move the external search at the end of the page instead of the right.

--- src/pages/food/FoodPage.svelte
+++ src/pages/food/FoodPage.svelte
@@ -88,87 +88,83 @@
 </script>
 
 <style>
-  section, .actions {
+  section, aside, .actions {
     margin-bottom: 16px;
   }
 </style>
 
 <Page>
   {#if food}
-    <TwoColumns gap="16px">
-      <div>
-        <UpdatableSection on:save={savePrimaryInformation} on:cancel={cancelPrimaryInformation}>
+    <UpdatableSection on:save={savePrimaryInformation} on:cancel={cancelPrimaryInformation}>
+      <div slot="view">
+        <PrimaryInformationSection food={food}/>
+      </div>
+
+      <div slot="edit">
+        <PrimaryInformationForm bind:food={foodForm}/>
+      </div>
+    </UpdatableSection>
+
+    {#each sections as section, index}
+      <section>
+        <UpdatableSection
+          initialState={section.initial === undefined ? "edit" : "view"}
+          on:save={() => saveSection(index)}
+          on:cancel={() => cancelSection(index)}
+        >
           <div slot="view">
-            <PrimaryInformationSection food={food}/>
+            <SectionView section={section.initial}/>
           </div>
 
           <div slot="edit">
-            <PrimaryInformationForm bind:food={foodForm}/>
+            <SectionForm
+              bind:section={sections[index].editing}
+              id={`food-section-${index}`}
+              name={`section[${index}]`}
+            />
           </div>
+
+          <span slot="actions">
+            <Button danger on:click={() => removeSection(index)}>
+              &#x1F5D1 {$i18n.t("pages.food.page.actions.removeSection")}
+            </Button>
+          </span>
         </UpdatableSection>
+      </section>
+    {/each}
 
-        {#each sections as section, index}
-          <section>
-            <UpdatableSection
-              initialState={section.initial === undefined ? "edit" : "view"}
-              on:save={() => saveSection(index)}
-              on:cancel={() => cancelSection(index)}
-            >
-              <div slot="view">
-                <SectionView section={section.initial}/>
-              </div>
-
-              <div slot="edit">
-                <SectionForm
-                  bind:section={sections[index].editing}
-                  id={`food-section-${index}`}
-                  name={`section[${index}]`}
-                />
-              </div>
-
-              <span slot="actions">
-                <Button danger on:click={() => removeSection(index)}>
-                  &#x1F5D1 {$i18n.t("pages.food.page.actions.removeSection")}
-                </Button>
-              </span>
-            </UpdatableSection>
-          </section>
-        {/each}
-
-        {#if !hasUnsavedAddedSection}
-          <div class="actions">
-            <Button type="primary" on:click={addSection}>{$i18n.t("pages.food.page.actions.addSection")}</Button>
-          </div>
-        {/if}
-
-        {#if nonEmpty(usedFor)}
-          <section>
-            <h2>{$i18n.t("pages.food.page.usedFor")}</h2>
-
-            <Grid>
-              {#each usedFor as info}
-                <RecipeCard recipe={info.recipe} food={info.food}/>
-              {/each}
-            </Grid>
-          </section>
-        {/if}
-
-        <div class="actions">
-          <Button danger on:click={() => deleteFood(id, $i18n)}>{$i18n.t("pages.food.page.actions.delete")}</Button>
-        </div>
+    {#if !hasUnsavedAddedSection}
+      <div class="actions">
+        <Button type="primary" on:click={addSection}>{$i18n.t("pages.food.page.actions.addSection")}</Button>
       </div>
+    {/if}
 
-      <aside>
-        {#each $receptoStore.searches as search}
-          <Collapsable summary={$i18n.t("pages.food.page.asideSearch", { sitename: search.sitename })}>
-            <iframe src={search.url.replace("{s}", food.name)}
-                    title={$i18n.t("pages.food.page.asideSearch", { sitename: search.sitename })}
-                    width="100%"
-                    height="500px"></iframe>
-          </Collapsable>
-        {/each}
-      </aside>
-    </TwoColumns>
+    {#if nonEmpty(usedFor)}
+      <section>
+        <h2>{$i18n.t("pages.food.page.usedFor")}</h2>
+
+        <Grid>
+          {#each usedFor as info}
+            <RecipeCard recipe={info.recipe} food={info.food}/>
+          {/each}
+        </Grid>
+      </section>
+    {/if}
+
+    <aside>
+      {#each $receptoStore.searches as search}
+        <Collapsable summary={$i18n.t("pages.food.page.asideSearch", { sitename: search.sitename })}>
+          <iframe src={search.url.replace("{s}", food.name)}
+                  title={$i18n.t("pages.food.page.asideSearch", { sitename: search.sitename })}
+                  width="100%"
+                  height="500px"></iframe>
+        </Collapsable>
+      {/each}
+    </aside>
+
+    <div class="actions">
+      <Button danger on:click={() => deleteFood(id, $i18n)}>{$i18n.t("pages.food.page.actions.delete")}</Button>
+    </div>
   {:else}
     <h1>{$i18n.t("common.notFound")}</h1>
   {/if}
